738978dd5945bc46f75c30afe028187081501a0f
Harys Vizcaino <harysvizcaino@gmail.com>
2022-01-03 11:18:39 -0400

    Added dynamic router and components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,33 @@
 import Head from 'next/head'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import Link from 'next/link'
+
+import Footer from '../components/Footer';
 
 export default function Home(initialData) {
+  const [formInputs, setFormInputs] = useState()
+  const [searchResults, setSearchResults] = useState([])
+  const [searchTerm, setSearchTerm] = useState('cats')
   useEffect(()=>{
-    console.log(initialData);
-  })
+    setSearchResults(initialData.catGiphys.data)
+  }, [initialData])
+
+  const handleInputs = (event) => {
+    let {name, value} = event.target
+    setFormInputs({ ...formInputs, [name]: value });
+  }
+
+  const search = async (event) => {
+    event.preventDefault()
+    let giphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=${formInputs.searchTerm}&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6`)
+    giphys = await giphys.json()
+    console.log({ giphys })
+    setSearchResults(giphys.data)
+    setSearchTerm(formInputs.searchTerm)
+  }
 
   return (
+    <>
     <div className='container'>
       <Head>
         <title>Create Next App</title>
@@ -14,13 +35,22 @@ export default function Home(initialData) {
         <link rel="stylesheet" href="/styles.css"/>
       </Head>
       <h1>Giphy Search App</h1>
-      <form>
-        <input type="text" />
+      <form onSubmit={search}>
+        <input name="searchTerm" onChange={handleInputs} type="text"/>
+        <button>Search</button>
       </form>
+      <h1>Search results for: {searchTerm}</h1>
+      <Link
+            href="/search/[pid]"
+            as={`/search/${searchTerm}`}>
+              <a> 
+                {`http://localhost:3000/search/${searchTerm}`}
+              </a>
+      </Link>
       <div className="giphy-search-results-grid">
-      { initialData.catGiphys.data.map((each, index) => {
+      { searchResults.map((each, index) => {
           return (
-            <div key="index">
+            <div key={index}>
               <h3>{each.title}</h3>
               <img src={each.images.original.url} alt={each.title} />
             </div>
@@ -29,10 +59,12 @@ export default function Home(initialData) {
       }
       </div>
     </div>
+    <Footer />
+    </>
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   let catGiphys = await fetch('https://api.giphy.com/v1/gifs/search?q=cats&api_key=2pZxLEROBssFqYXsjMQMyNPwilFH16q0&limit=10')
   catGiphys = await catGiphys.json()
   return {props: {catGiphys: catGiphys}} 
